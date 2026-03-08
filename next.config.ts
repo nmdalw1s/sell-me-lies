b7add65aba10c64f0541450e901db21dcfed5a1d
neth <neth@Mac.lan>
2026-03-07 22:46:44 -0500

    Fix Github Pages Base Path

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
+  basePath: isProd ? "/sell-me-lies" : "",
+  assetPrefix: isProd ? "/sell-me-lies/" : "",
 };
 
-module.exports = nextConfig;
+export default nextConfig;
